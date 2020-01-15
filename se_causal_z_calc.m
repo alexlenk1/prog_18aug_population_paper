a090739bd97e733d25dc04623c0d35ac497dfe29
alexlenk1 <30493518+alexlenk1@users.noreply.github.com>
2020-01-15 13:12:50 -0800

    Changes

--- se_causal_z_calc.m
+++ se_causal_z_calc.m
@@ -1,38 +1,28 @@
-function se_causal_z=se_causal_z_calc(Y,X,Z,beta_tilde,rho,N)
+function [se_ehw,se_desc,se_causal_sample,se_causal]=se_causal_z_calc(Y,X,Z,beta,rho,N)
 
-% This function outputs estimates for several variance estimators given in the paper 
+% This function outputs estimates for several variance estimators given in the paper (see p. 22-23) 
 
-% 1.Calculating the Eicker-Huber-White variance for coefficient on X (scalar)
+  H=X'*X; %denoted as capital Gamma in the paper 
 
-%eps=Y-[U Z]*beta; NOT CONSISTENT WITH definition on p.22
+  eps = Y-[X Z]*beta %calculating regression residuals (as on p.22) 
+  Xeps=X.*eps;
+  G=(inv(Z'*Z)*(Z'*Xeps));
 
-H=X'*X; %denoted as capital Gamma in the paper 
-eps = Y-[X Z]*beta_tilde %calculating regression residulas 
-Xeps=X.*eps;
-G=(inv(Z'*Z)*(Z'*Xeps));
+  Delta_ehw=(Xeps'*Xeps)/N;
+  Delta_z=(Xeps-Z*G)'*(Xeps-Z*G)/N;
 
-Delta_ehw=(Xeps'*Xeps)/N;
-Delta_z=(Xeps-Z*G)'*(Xeps-Z*G)/N;
-Delta_causal_z=rho*Delta_z+(1-rho)*Delta_ehw;
-
-
-V_ehw=inv(H)*Delta_ehw*inv(H); % This is the estimated sqrt(N) variance
-V_desc = (1-rho)*V_ehw;
-V_causal_sample=inv(H)*Delta_z*inv(H);
-V_causal=inv(H)*Delta_causal_z*inv(H);
-
-se_ehw=sqrt(V_ehw);
-se_hat_desc=sqrt(V_desc)
-se_causal_z=sqrt(V_causal_z);
-se_causal_sample_z = sqrt(
+  % These are estimated sqrt(N) variances
+  V_ehw=inv(H)*Delta_ehw*inv(H);
+  V_desc = (1-rho)*V_ehw;
+  V_causal_sample=inv(H)*Delta_z*inv(H);
+  V_causal=rho*V_causal_sample + (1-rho)*V_ehw;
 
 
-
-V_z=inv(H)*Delta_z*inv(H);
-V_causal_z=inv(H)*Delta_causal_z*inv(H);
-
-
-se_causal_z=sqrt(V_causal_z);
-
+  % We now calculated the estimated variances of the estimators (no longer sqrt(N))
+  se_ehw=sqrt(V_ehw/N);
+  se_desc=sqrt(V_desc/N);
+  se_causal=sqrt(V_causal/N);
+  se_causal_sample = sqrt(V_causal_sample/N);
+  
 end
 

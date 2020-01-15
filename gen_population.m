727c30efd6bca1c86e86ac3b614ec3d29e88134b
alexlenk1 <30493518+alexlenk1@users.noreply.github.com>
2020-01-15 15:14:47 -0800

    Change function name

--- gen_population.m
+++ gen_population.m
@@ -1,9 +1,9 @@
 
 function [Z,xi,theta]=gen_population(n,psi,gamma,lambda,sig_theta,sig_xi,KZ);
 
-% generating population data
+% This function generates population data.
 
-%input
+%Input
 %n = population size
 %psi = parameter determining the dependence between Z and theta
 %gamma = parameter determining the dependence between Z and xi
@@ -12,15 +12,16 @@ function [Z,xi,theta]=gen_population(n,psi,gamma,lambda,sig_theta,sig_xi,KZ);
 %sig_xi = sd of xi
 %KZ = dimension of Z, excluding constant term
 
-%output 
+%Output 
 %Z = n by (KZ+1) matrix of fixed attributes for population n
 %xi = the intercept vector of size n in the potential outcome function
 %theta = treatment effect vector of size n  in the potential outcome function
 
-Z=[ones(n,1),randn(n,KZ)];  
+  Z=[ones(n,1),randn(n,KZ)];  
 
-xi_error=randn(n,1)*sig_xi; % xi_error is N(0, (sig_xi)^2 )
-theta_error=randn(n,1)*sig_theta; % theta_error is N(0, (sig_theta)^2 )
+  xi_error=randn(n,1)*sig_xi; % xi_error is N(0, (sig_xi)^2 )
+  theta_error=randn(n,1)*sig_theta; % theta_error is N(0, (sig_theta)^2 )
 
-xi=Z*gamma+xi_error;  % intercept for potential outcomes, distributed as N(Z*gamma, var(xi_error) ) 
-theta=Z*psi+theta_error;      % treatment effect, distributed as N(Z*psi, var(theta_error) ) 
+  xi=Z*gamma+xi_error;  % intercept for potential outcomes, distributed as N(Z*gamma, var(xi_error) ) 
+  theta=Z*psi+theta_error;      % treatment effect, distributed as N(Z*psi, var(theta_error) ) 
+  

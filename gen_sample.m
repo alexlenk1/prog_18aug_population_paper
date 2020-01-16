1395f0448aeea4b3904c489960a12fd498e74938
alexlenk1 <30493518+alexlenk1@users.noreply.github.com>
2020-01-15 20:12:46 -0800

    Changes

--- gen_sample.m
+++ gen_sample.m
@@ -1,11 +1,10 @@
 
-function       [YR,XR,ZR,UR,R]=gen_sample(rho,Y,X,Z,U);
+function       [YR,ZR,UR,R]=gen_sample(rho,Y,X,Z,U)
 
-% generating sample data
+% This function samples data for the population and creates the sample for each simulation
  
-R=rand(n,1)<=rho;  % sampling indicator
+ R=rand(n,1)<=rho;  % sampling indicator
 
-YR=Y(R,:);         % sampled outcome
-XR=X(R,:);         % sampled adjusted treatment
-ZR=Z(R,:);         % sampled features
-UR=U(R,:);         % sampled treatment
+ YR=Y(R,:);         % sampled outcome
+ ZR=Z(R,:);         % sampled features
+ UR=U(R,:);         % sampled treatment

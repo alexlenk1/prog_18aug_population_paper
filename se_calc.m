efcf9bb340a78755723afed15c3c8611b147d1d1
alexlenk1 <30493518+alexlenk1@users.noreply.github.com>
2020-01-15 22:08:10 -0800

    Changes

--- se_calc.m
+++ se_calc.m
@@ -2,14 +2,14 @@ function [se_ehw,se_desc,se_causal_sample,se_causal]=se_calc(Y,X,Z,beta,rho,N)
 
 % This function outputs estimates for several variance estimators given in the paper (see p. 22-23) 
 
-  H=X'*X; %denoted as capital Gamma in the paper 
+  H=(X'*X)/N; %denoted as capital Gamma in the paper 
 
   eps = Y-[X Z]*beta %calculating regression residuals (as on p.22) 
   Xeps=X.*eps;
   G=(inv(Z'*Z)*(Z'*Xeps));
 
   Delta_ehw=(Xeps'*Xeps)/N;
-  Delta_z=(Xeps-Z*G)'*(Xeps-Z*G)/N;
+  Delta_z=((Xeps-Z*G)'*(Xeps-Z*G))/N;
 
   % These are estimated sqrt(N) variances
   V_ehw=inv(H)*Delta_ehw*inv(H);
